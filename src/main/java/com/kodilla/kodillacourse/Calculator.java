7ed1a31904262d9c8f498e616bda293938c605f7
unknown <bblizniewska.1988@gmail.com>
2019-06-05 22:40:44 +0200

    First Commit

---
+++ src/main/java/com/kodilla/kodillacourse/Calculator.java
@@ -0,0 +1,12 @@
+package com.kodilla.kodillacourse;
+
+public class Calculator {
+    public void someMethod(){
+    System.out.println("It's work!");
+    }
+    public static void main(String args[]){
+        Calculator calculator = new Calculator();
+        calculator.someMethod();
+    }
+
+}
